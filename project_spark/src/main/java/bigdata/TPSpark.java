07ba154eba33a517d1ef473fb0ae44bb1aeda4d7
LucasVivas <lucas.vivas33@gmail.com>
2019-01-22 16:23:08 +0100

    modif

--- project_spark/src/main/java/bigdata/TPSpark.java
+++ project_spark/src/main/java/bigdata/TPSpark.java
@@ -1,7 +1,9 @@
 package bigdata;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.input.PortableDataStream;
 
 public class TPSpark {
 
@@ -9,7 +11,15 @@ public class TPSpark {
 		
 		SparkConf conf = new SparkConf().setAppName("Projet BIGdata");
 		JavaSparkContext context = new JavaSparkContext(conf);
-		context.sequenceFile();
+
+		String path00 = "hgt/N43W001.hgt";
+		String path01 = "hgt/N43W002.hgt";
+		String path10 = "hgt/N44W001.hgt";
+		String path11 = "hgt/N44W002.hgt";
+
+		JavaPairRDD<String, PortableDataStream> rdd = context.binaryFiles
+				("hdfs://young:9000/user/pascal/dem3seq/N43W001.hgt");
+
 
 	}
 	
