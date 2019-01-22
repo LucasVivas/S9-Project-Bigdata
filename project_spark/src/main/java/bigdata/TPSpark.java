bac84ddb84d91fdc7d7d8547b0836c79f3267879
AlexCasanova <acasanova33560@hotmail.fr>
2019-01-22 16:21:22 +0100

    double boucle for

--- project_spark/src/main/java/bigdata/TPSpark.java
+++ project_spark/src/main/java/bigdata/TPSpark.java
@@ -6,11 +6,31 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class TPSpark {
 
 	public static void main(String[] args) {
-		
+
 		SparkConf conf = new SparkConf().setAppName("Projet BIGdata");
 		JavaSparkContext context = new JavaSparkContext(conf);
 		context.sequenceFile();
 
+		const int sizeX = 1200;
+		const int sizeY = 1200;
+		const int TILEX = 75;
+		const int TILEY = 75;
+		const int maxNbTileX = sizeX/TILEX;
+		const int maxNbTileY = sizeY/TILEY;
+
+		for(int caseY=0; caseY<maxNbTileY; caseY++){
+			for(int caseX=0; caseX<maxNbTileX; caseX++){
+
+				for(int y=caseY*TILEY; y<(caseY+1)*TILEY; y++){
+					for(int x=caseX*TILEX; x<(caseX+1)*TILEX; x++){
+
+						tab[y*1201 + x]
+					}
+				}
+
+			}
+		}
+
 	}
-	
+
 }
